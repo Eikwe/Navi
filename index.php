efdc0978dc3fc290febf951e29d7af6adaf9fdca
Projekt <125804811+Eikwe@users.noreply.github.com>
2023-06-27 19:05:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,86 @@
+<html !DOCTYPE html> 
+<title>Partnerschaft Eikwe-Schweinfurt</title>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GE34BCJEDH"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-GE34BCJEDH');
+    </script>
+<body> 
+
+<head>
+  <title>Meine Webseite</title>
+</head>
+<body>
+  Hallo
+<?php include 'navi.php'; ?>
+  <link href="style.css" rel="stylesheet">
+  
+  <script src="navifooter.js"></script>
+   <script src="index.js"></script> 
+<div id="slider">
+  <figure>
+    <img src="Titelbild.jpg">
+    <img src="Schaukasten.jpg">
+    <img src="GesamtbildSW.jpg">
+    <img src="Solar.jpg">
+    <img src="Ubuntu.jpg">
+    <img src="Advent.jpeg">
+    <img src="Marktplatz.jpeg">
+    
+  </figure>
+  <div id="slider-nav">
+    <div id="prev-slide">&lt;</div>
+    <div id="next-slide">&gt;</div>
+  </div>
+  <center>
+  <div id="slider-controls">
+    <input type="radio" name="slider-radio" id="radio1" checked>
+    <input type="radio" name="slider-radio" id="radio2">
+    <input type="radio" name="slider-radio" id="radio3">
+    <input type="radio" name="slider-radio" id="radio4">
+    <input type="radio" name="slider-radio" id="radio5">
+    <input type="radio" name="slider-radio" id="radio6">
+    <input type="radio" name ="slider-radio" id="radio7">
+ 
+  </div>
+</div>
+ </center>
+
+<br>
+   
+      <h2> <b> Herzlich Willkommen!</b> </h2>
+      
+      Auf dieser Seite finden Sie Informationen zu den Projekten des P-Seminars der Walther-Rathenau Schulen. <br> <br>
+      <div class="background">
+  <h2 style="margin-top: 1vw;"> <u>   Kabelsammlung für unser Projekt  </h2> </u>  
+  <div class="containerabpx" > 
+  <img class="bild-sonderfall" src="pseminar.jpg"> 
+  <p>Aktuell sammeln wir alte und kaputte Kabel ein, die wir gemeinsam mit den Schweinfurter Organisationen "People4Future" und "Agenda2030" zum Recyceln geben. <br>
+    Das durch diese Aktion gesammelte Geld kommt unserem Projekt, dem Bau einer Solaranlage für die St. Luke's School in Ghana, zugute. <br>
+    Gerne können Sie am <b> 13. Mai von 10- 15 Uhr </b> Ihre kaputten Kabel auf dem <b>Schweinfurter Marktplatz </b> abgeben. <br>
+    Diese Aktion findet im Rahmen der "Zukunftswoche Mainfranken" statt.
+     <br> </div>  </p>
+  
+  <center> <div class="container"> 
+    <a href="https://zukunftswoche-mainfranken.de" target="_blank">  <button> Zukunftswoche Mainfranken - Website  </button>  </a> 
+  <a href="FlyerZukunft.pdf" target="_blank"> <button> Zukunftswoche Mainfranken Flyer </button> </a> <br> <br> </div> 
+<br> </div> </center>
+     
+  
+    <div class="backgroundblue" > <br><u> <h1> UNSERE PROJEKTE </h1> </u>
+    <div class="icons"> 
+      <a href="Solarprojekt.html"><img src="Solar-IconD.png">   Solarprojekt </a> 
+      <a href="kunstprojekt.html"> <img  src="Kunstprojekt-IconD.png">  Kunstprojekt </a>  
+      <a href="Partnerschaft.html"><img src="Partnerschaft-IconD.png"> Partnerschaft </a> 
+      </div>
+   
+     
+    <br>
+</div>
+<div id="fenster"></div>
+  <div id="footer"></div>
+
+</html>
